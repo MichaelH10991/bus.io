596255dd6891067af5050168bab24f3acee2223e
Michael Humphries <michaelhumphries1997@gmail.com>
2020-03-08 20:13:10 +0000

    debugging mongoose connection

--- api/src/connection.js
+++ api/src/connection.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 import "regenerator-runtime/runtime";
 const url = process.env.MONGO_CONNECT_URL;
 const database = process.env.MONGO_DATABASE;
-const connection_url = `${url}/${database}`
-console.log(connection_url)
+// const connection_url = `${url}/${database}`
+const connection_url = "mongodb://master:Copella123!@localhost:27017/busses"
 //https://www.freecodecamp.org/news/introduction-to-mongoose-for-mongodb-d2a7aa593c57/
 
 class Database {
@@ -14,27 +14,25 @@ class Database {
 
 async _connect() {
     if (url) {
+      try{
           console.log(`Starting connection to ${connection_url} database...`);
-          try {
             await mongoose.connect(connection_url, {
               useNewUrlParser: true,
-              useUnifiedTopology: true
+              useUnifiedTopology: false
             });
             const db = mongoose.connection
             await db.on('error', console.error.bind(console, 'connection error:'))
             await db.once('open', () => {
               console.log(`bus_api connected to ${connection_url} database.`);
             })
-          } catch (error) {
-            console.log('other error: ', error);
+          }catch(e){
+              console.log(e)
           }
-          
       } else {
         console.log(
           "Mongoose connection not created because connection url has not been set."
         );
       }
-    
 }
 }
 
